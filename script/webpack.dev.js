fbae8e5a6aa1973c0b40cd4aba1e55fd28e80dfa
GavinHe322 <https://github.com/GavinHe322>
2019-12-20 13:43:23 +0800

    fix: 修复代码格式

--- script/webpack.dev.js
+++ script/webpack.dev.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ProgressBarWebpackPlugin = require('progress-bar-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ProgressBarWebpackPlugin = require("progress-bar-webpack-plugin")
 
-const { VueLoaderPlugin } = require('vue-loader');
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    'marmot-mobile':  './docs/site/mobile/main.js',
-    'marmot-docs':  './docs/site/desktop/main.js',
+    "marmot-mobile":  "./docs/site/mobile/main.js",
+    "marmot-docs":  "./docs/site/desktop/main.js",
   },
   output: {
-    path: path.join(__dirname, '../docs/dist'),
-    publicPath: '/',
-    chunkFilename: 'async_[name].js'
+    path: path.join(__dirname, "../docs/dist"),
+    publicPath: "/",
+    chunkFilename: "async_[name].js",
   },
   module: {
     rules: [
@@ -21,86 +21,86 @@ module.exports = {
         test: /\.vue$/,
         use: [
           {
-            loader: 'vue-loader',
+            loader: "vue-loader",
             options: {
               compilerOptions: {
-                preserveWhitespace: false
-              }
-            }
-          }
-        ]
+                preserveWhitespace: false,
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'eslint-loader',
-        enforce: 'pre'
+        use: "eslint-loader",
+        enforce: "pre",
       },
       {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader",
       },
       {
         test: /\.(less|css)$/,
         sideEffects: true,
         use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
           {
-            loader: 'less-loader',
+            loader: "less-loader",
             options: {
-              paths: [path.resolve(__dirname, 'node_modules')]
-            }
-          }
-        ]
+              paths: [path.resolve(__dirname, "node_modules")],
+            },
+          },
+        ],
       },
       {
         test: /\.md$/,
         use: [
           {
-            loader: 'vue-loader',
+            loader: "vue-loader",
           },
           {
-            loader: 'vue-markdown-loader/lib/markdown-compiler',
+            loader: "vue-markdown-loader/lib/markdown-compiler",
             options: {
-              raw: true
-            }
-          }
-        ]
-      }
-    ]
+              raw: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.vue', '.less'],
+    extensions: [".js", ".ts", ".tsx", ".vue", ".less"],
     alias: {
-      '@': path.join(__dirname, '../src')
+      "@": path.join(__dirname, "../src"),
     },
   },
   stats: {
-    children: false
+    children: false,
   },
   devServer: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     open: true,
-    stats: 'errors-only',
+    stats: "errors-only",
   },
   plugins: [
     new VueLoaderPlugin(),
     new ProgressBarWebpackPlugin(),
     new HtmlWebpackPlugin({
-      chunks: ['marmot-mobile'],
-      template: 'docs/site/mobile/index.html',
-      filename: 'mobile.html',
+      chunks: ["marmot-mobile"],
+      template: "docs/site/mobile/index.html",
+      filename: "mobile.html",
       inject: true,
     }),
     new HtmlWebpackPlugin({
-      chunks: ['marmot-docs'],
-      template: 'docs/site/desktop/index.html',
-      filename: 'index.html',
+      chunks: ["marmot-docs"],
+      template: "docs/site/desktop/index.html",
+      filename: "index.html",
       inject: true,
     }),
-   
-  ]
+
+  ],
 }
--- script/webpack.doc.js
+++ script/webpack.doc.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const config = require('./webpack.dev.js');
+const path = require("path");
+const config = require("./webpack.dev.js");
 
 module.exports = Object.assign(config, {
-  mode: 'production',
+  mode: "production",
   output: {
-    path: path.join(__dirname, '../dist'),
-    filename: '[name].[hash:8].js',
-    chunkFilename: 'async_[name].[chunkhash:8].js'
-  }
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].[hash:8].js",
+    chunkFilename: "async_[name].[chunkhash:8].js",
+  },
 });
