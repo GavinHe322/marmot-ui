faeb896799aa71d61e871f4312b8c298c3481bbe
GavinHe322 <https://github.com/GavinHe322>
2019-12-20 11:05:23 +0800

    chore: 增加打包功能, 测试 travis.cli 持续继承

---
+++ script/webpack.doc.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const config = require('./webpack.dev.js');
+
+module.exports = Object.assign(config, {
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[hash:8].js',
+    chunkFilename: 'async_[name].[chunkhash:8].js'
+  }
+});
